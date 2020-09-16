1a7f91efba576dd4b03b4f6c761f5dfb441a842c
ghfbsd <ghfbsd@gmail.com>
2020-09-16 10:33:20 +0100

    IBM360: Fix NOP treatment of SADxxx cmds

--- IBM360/ibm360_com.c
+++ IBM360/ibm360_com.c
@@ -272,7 +272,7 @@ uint8  coml_startcmd(UNIT *uptr, uint16 chan,  uint8 cmd) {
 
     switch (cmd & 0x3) {
     case 0x3:              /* Control */
-         if ((cmd == CMD_NOP) || (cmd & 0x08))
+         if ((cmd == CMD_NOP) || (cmd & 0x10))
              /* 2703 treats 2701 SADxxx commands as NOP */
              return SNS_CHNEND|SNS_DEVEND;
     case 0x2:              /* Read command */
